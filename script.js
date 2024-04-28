8451a4eb29cb4bfe9badf911f174f14e3c4c6c26
yladev <120935093+yladev@users.noreply.github.com>
2024-04-28 18:04:43 +0630

    setup project

---
+++ script.js
@@ -0,0 +1,9 @@
+const apikey = "4ab69bfdfcac15e6e5099dfdd5a189e5";
+
+const weatherData = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener();
