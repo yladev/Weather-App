551d44e36021765951d5df51b411647b5ab231ce
yladev <120935093+yladev@users.noreply.github.com>
2024-05-01 23:43:46 +0630

    update js file

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 const apikey = "4ab69bfdfcac15e6e5099dfdd5a189e5";
 
-const weatherData = document.getElementById("weather-data");
+const weatherDataEl = document.getElementById("weather-data");
 
 const cityInputEl = document.getElementById("city-input");
 
 const formEl = document.querySelector("form");
 
-formEl.addEventListener();
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityValue = cityInputEl.value;
+  // console.log(cityValue);
+  getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+
+    // console.log(data);
+
+    const temperature = Math.round(data.main.temp);
+
+    const description = data.weather[0].description;
+
+    const icon = data.weather[0].icon;
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}`,
+      `Wind speed: ${data.wind.speed}`,
+    ];
+
+    weatherDataEl.querySelector(
+      ".icon"
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+
+    weatherDataEl.querySelector(
+      ".temperature"
+    ).textContent = `${temperature}°C`;
+
+    weatherDataEl.querySelector(".description").textContent = description;
+  } catch (error) {}
+}
